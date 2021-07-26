a587db8170c45f6fc31324cb534b83721d741cfe
dimitar1128 <dimi.rangev09@gmail.com>
2021-07-26 15:07:53 +0000

    - test code

--- sectors/common/mail.py
+++ sectors/common/mail.py
@@ -12,6 +12,10 @@ from db.models import (
 
 def test_smtp(request, smtp_setting):
     try:
+        print(smtp_setting)
+        print(smtp_setting['smtp_server_name'])
+        print(smtp_setting['smtp_port'])
+        print(smtp_setting['smtp_enable_starttls'])
         s = smtplib.SMTP(host=smtp_setting['smtp_server_name'], port=int(smtp_setting['smtp_port']))
         s.ehlo()
         if smtp_setting['smtp_enable_starttls']:
